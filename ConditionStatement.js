fe0cc948d7f04df8a2de4882570be1d261c0dc29
muhammedarshadtp <arshadtp89@gmail.com>
2025-08-12 13:42:32 +0530

    Add examples of conditional statements in JavaScript
    - Implemented if, if-else, and if-else-if statements
    - Demonstrated switch-case statement with default case
    - Used ternary operator for concise conditional logic
    - Added console outputs to show results of each condition

---
+++ ConditionStatement.js
@@ -0,0 +1,38 @@
+
+//  if/else statement
+
+let x = 5
+
+if(x>10){
+    console.log("1")
+}else if(x<5){
+    console.log("2")
+}else{
+    console.log("5")
+}
+
+
+// Ternary Operator
+
+let y=10
+
+let z = y>10 ? "1":"0"
+
+console.log(z)
+
+// Switch statement
+
+
+let a=10
+
+switch (a) {
+    case 1:
+        console.log("1")
+        break;
+    case 10:
+        console.log("10")
+        break;
+    default:
+        console.log("0")
+        break;
+}
