48fcc5f679322ba155f6a8b70b8fc62a9844f677
muhammedarshadtp <arshadtp89@gmail.com>
2025-08-23 00:42:29 +0530

    precticing for iife function and working

--- Functions.js
+++ Functions.js
@@ -1,82 +1,91 @@
-// Normal function
-function sum(a, b) {
-    return a + b
-}
-console.log(sum(4, 3))
+// // Normal function
+// function sum(a, b) {
+//     return a + b
+// }
+// console.log(sum(4, 3))
 
-// Anonymous function
+// // Anonymous function
 
-console.log(function (a, b) {
-    return a * b
-}(4, 5))
+// console.log(function (a, b) {
+//     return a * b
+// }(4, 5))
 
-// function expression
+// // function expression
 
-let add = function (a, b) {
-    return a * b
-}
+// let add = function (a, b) {
+//     return a * b
+// }
 
-console.log(add(4, 5));
+// console.log(add(4, 5));
 
-// Arrow Function
+// // Arrow Function
 
-let arrow = (a, b) => a * b
-console.log(arrow(2, 3))
+// let arrow = (a, b) => a * b
+// console.log(arrow(2, 3))
 
-// callback function
-function sum(a, b) {
-    return a + b
-}
-sum(2, 3)
-function sum2(a, b, operater) {
-    let m = operater(a, b)
-    console.log(m)
-}
-sum2(10, 5, sum)
+// // callback function
+// function sum(a, b) {
+//     return a * b
+// }
+// sum(2, 3)
+// function sum2(a, b, operater) {
+//     let m = operater(a, b)
+//     console.log(m)
+// }
+// sum2(10, 5, sum)
 
-// pure and impure function
+// // pure and impure function
 
-// pure(same output)
-function sum(a, b) {
-    return a + b
-}
-sum(2, 3)
-
-
-// impure (not same output)
-
-let total = 0
-
-function impure(value) {
-    total += value
-    return total
-}
-console.log(impure(5));
-console.log(impure(5));
-
-// currying
-
-function curry(a) {
-    return function (b) {
-        return a * b
-    }
-}
-let double = curry(2)
-console.log(double(3))
-
-// closure
-function a() {
-    for (var i = 1; i <= 5; i++) {
-        function close(x) {
-            setTimeout(function () {
-                console.log(x)
-
-            },x* 1000)
-        }
-        close(i)
-    }
-    console.log("hello arshad")
-}
-a()
+// // pure(same output)
+// function sum(a, b) {
+//     return a + b
+// }
+// sum(2, 3)
 
 
+// // impure (not same output)
+
+// let total = 0
+
+// function impure(value) {
+//     total += value
+//     return total
+// }
+// console.log(impure(5));
+// console.log(impure(5));
+
+// // currying
+
+// function curry(a) {
+//     return function (b) {
+//         return a * b
+//     }
+// }
+// let double = curry(2)
+// console.log(double(3))
+
+// // closure
+// function a() {
+//     for (var i = 1; i <= 5; i++) {
+//         function close(x) {
+//             setTimeout(function () {
+//                 console.log(x)
+
+//             },x* 1000)
+//         }
+//         close(i)
+//     }
+//     console.log("hello arshad")
+// }
+// a()
+
+
+const counterIIFE = (function () {
+  let count = 0; // private
+  return function () {
+    return ++count;
+  };
+})();
+console.log(counterIIFE()); // 1
+console.log(counterIIFE()); // 2
+console.log(counterIIFE()); // 3
